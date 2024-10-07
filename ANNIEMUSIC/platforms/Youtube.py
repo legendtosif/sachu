f7902f969558e69d5e5e0da2e94306c5f347cd06
DeadliestOne <uncountableaura@gmail.com>
2024-10-07 22:33:27 +0530

    Update Youtube.py

--- ANNIEMUSIC/platforms/Youtube.py
+++ ANNIEMUSIC/platforms/Youtube.py
@@ -9,7 +9,7 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from ANNIEUSIC.utils.database import is_on_off
+from ANNIEMUSIC.utils.database import is_on_off
 from ANNIEMUSIC.utils.formatters import time_to_seconds
 
 
